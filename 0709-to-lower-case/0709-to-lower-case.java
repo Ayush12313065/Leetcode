05e627489168ecc878eb38ac5556838655dbde36
Ayush04 <152521754+Ayush12313065@users.noreply.github.com>
2026-07-02 12:29:00 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.05%) - LeetHub

--- 0709-to-lower-case/0709-to-lower-case.java
+++ 0709-to-lower-case/0709-to-lower-case.java
@@ -1,14 +1,6 @@
 class Solution {
     public String toLowerCase(String s) {
-        StringBuilder string  = new StringBuilder();
-        for (char c :s.toCharArray()){
-            if(Character.isUpperCase(c)){
-                string.append(Character.toLowerCase(c));
-            }
-            else{
-                string.append(c);
-            }
-        }
-        return string.toString();
+        
+        return s.toLowerCase();
     }
 }
